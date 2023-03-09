fbad748c27508221e6c937968d3ac48a57a53e6e
Adam Baldwin <adi.baldwin@hotmail.com>
2023-03-09 23:00:02 +0000

    successfully registered user, logged in and got all users with token

--- src/users/controllers.js
+++ src/users/controllers.js
@@ -14,9 +14,11 @@ const registerUser= async (req, res) => {
 const login = async (req, res) => {
     try {
         const token = await jwt.sign({id: req.user.id}, process.env.SECRET_KEY);
-        res.status(202).json({
+        res.status(201).json({
             message: "successful",
-            user: {username: req.user.username, email: req.user.email, token: token},
+            user: {username: req.user.username, 
+            email: req.user.email, 
+            token: token},
         })
     } catch (error) {
         res.status(501).json({ errorMessage: error.message, error: error });
@@ -33,7 +35,11 @@ const getAllUsers = async (req, res) => {
         const users = await User.findAll();
         for (let user of users) {
             user.password = "";
-        }
+        } 
+        res.status(200).json({
+            message: "successful",
+            users: users
+        })
     } catch (error){
         res.status(501).json({ errorMessage: error.message, error: error });
     }
